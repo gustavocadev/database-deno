653b52164f4662a12311859dc5736d465b5a1a5a
gustavocadev <churaalaygustavo11@gmail.com>
2022-01-24 16:36:12 -0500

    I added a Deno args

--- server.ts
+++ server.ts
@@ -2,6 +2,7 @@ import { Application } from "https://deno.land/x/oak/mod.ts";
 import { Handlebars } from 'https://deno.land/x/handlebars/mod.ts';
 import routerPages from "./routes/renderPages.routes.ts";
 import routerUsers from './routes/users.routes.ts'
+import { parse } from 'https://deno.land/std/flags/mod.ts';
 
 const app = new Application();
 const handle = new Handlebars();
@@ -14,6 +15,10 @@ app.use(routerUsers.allowedMethods())
 
 
 console.log(`listening on port 3000...`);
-await app.listen({ port: 3000 });
+const DEFAULT_PORT = 3000
+
+const argPort = parse(Deno.args).port
+
+await app.listen({ port: argPort ? argPort : DEFAULT_PORT });
 
 export { handle }
