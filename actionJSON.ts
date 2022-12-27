414c7a4a2ee829fd9215ca1139a639ac06cb480d
gustavocadev <churaalaygustavo11@gmail.com>
2022-12-27 09:28:39 -0500

    update space_tab from 4 to 2

--- actionJSON.ts
+++ actionJSON.ts
@@ -1,48 +1,56 @@
 import * as path from 'https://deno.land/std/path/mod.ts';
 
-
 interface ReadJSON {
-    [propName: string]: Record<string, unknown>[]
+  [propName: string]: Record<string, unknown>[];
 }
 
 class ActionJSON {
+  constructor(
+    private pathAndFileName: string,
+    private entryPropertyName: string
+  ) {}
 
-    constructor(
-        private pathAndFileName: string,
-        private entryPropertyName: string
-    ) {}
-
-    async writeJSON(newArray: Record<string, unknown>[], indent = { indent: true }) {
-        const newData = {
-            [this.entryPropertyName]: newArray
-        }
-        // by default the indent is true
-        if (!indent.indent) {
-            await Deno.writeTextFile(path.join(Deno.cwd(), this.pathAndFileName), JSON.stringify(newData))
-            return
-        } 
-        // Write JSON 
-        await Deno.writeTextFile(path.join(Deno.cwd(), this.pathAndFileName), JSON.stringify(newData, null, 4))
+  async writeJSON(
+    newArray: Record<string, unknown>[],
+    indent = { indent: true }
+  ) {
+    const newData = {
+      [this.entryPropertyName]: newArray,
+    };
+    // by default the indent is true
+    if (!indent.indent) {
+      await Deno.writeTextFile(
+        path.join(Deno.cwd(), this.pathAndFileName),
+        JSON.stringify(newData)
+      );
+      return;
     }
+    // Write JSON
+    await Deno.writeTextFile(
+      path.join(Deno.cwd(), this.pathAndFileName),
+      JSON.stringify(newData, null, 4)
+    );
+  }
 
+  async readJSON(): Promise<ReadJSON> {
+    // We read the data
+    const data = await Deno.readTextFile(
+      path.join(Deno.cwd(), this.pathAndFileName)
+    );
 
-    async readJSON(): Promise<ReadJSON> {
-        // We read the data 
-        const data = await Deno.readTextFile(path.join(Deno.cwd(), this.pathAndFileName))
-        
-        // if there's no content we create a template :D
-        if (data.length === 0) {
-            const newData = {
-                [this.entryPropertyName]: []
-            }
-            await Deno.writeTextFile(path.join(Deno.cwd(), this.pathAndFileName), JSON.stringify(newData, null, 4))
-            return newData
-
-        }
-        return JSON.parse(data)
+    // if there's no content we create a template :D
+    if (data.length === 0) {
+      const newData = {
+        [this.entryPropertyName]: [],
+      };
+      await Deno.writeTextFile(
+        path.join(Deno.cwd(), this.pathAndFileName),
+        JSON.stringify(newData, null, 4)
+      );
+      return newData;
     }
-
+    return JSON.parse(data);
+  }
 }
 
-
-export { ActionJSON }
+export { ActionJSON };
--- routes/renderPages.routes.ts
+++ routes/renderPages.routes.ts
@@ -1,33 +1,34 @@
-import { Router } from "https://deno.land/x/oak/mod.ts";
-import { renderTeamPage, renderIndex } from "../controllers/renderPages.controllers.ts";
+import { Router } from 'https://deno.land/x/oak/mod.ts';
+import {
+  renderTeamPage,
+  renderIndex,
+} from '../controllers/renderPages.controllers.ts';
 import { handle } from '../server.ts';
-import { ActionJSON } from '../actionJSON.ts'
+import { ActionJSON } from '../actionJSON.ts';
 
 const router = new Router();
-const actionJSON = new ActionJSON('db.json', 'users')
+const actionJSON = new ActionJSON('db.json', 'users');
 
 // Renderizamos la vista del index
-router.get("/", renderIndex);
+router.get('/', renderIndex);
 
 // Renderizamos la vista de new-user
-router.get("/users/new-user", async (ctx) => {
-    ctx.response.body = await handle.renderView('new-user')
+router.get('/users/new-user', async (ctx) => {
+  ctx.response.body = await handle.renderView('new-user');
 });
 
+router.get('/team', renderTeamPage);
 
-router.get("/team", renderTeamPage);
+router.get('/users/edit-user/:id', async (ctx) => {
+  const { users } = await actionJSON.readJSON();
 
-router.get("/users/edit-user/:id", async (ctx) => {
-    const { users } = await actionJSON.readJSON()
+  const { id } = ctx.params;
 
-    const { id } = ctx.params
+  const userData = users.find((user) => user.id === id);
 
-    const userData = users.find(user => user.id === id)
-
-
-    ctx.response.body = await handle.renderView('edit-user', {
-        userData
-    })
-})
+  ctx.response.body = await handle.renderView('edit-user', {
+    userData,
+  });
+});
 
 export default router;
